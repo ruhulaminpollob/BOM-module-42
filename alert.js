9b6983c579545f0b48075c45982cfaf778a35781
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-11 13:32:39 +0600

    explore prompt

--- alert.js
+++ alert.js
@@ -6,5 +6,11 @@ showAlert=()=>{
 
 showConfirm=()=>{
     const confirmResult=confirm('Are you agree to our terms and conditions');
-    console.log(confirmResult);
+    // console.log(confirmResult); confirm returns true and false
+}
+
+showPrompt=()=>{
+    const promptResult=prompt('What is your nike name?');
+    // console.log(promptResult); prompt returns input value on okay click and  returns  null on cancel click
+    
 }
